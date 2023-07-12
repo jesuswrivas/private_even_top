93094e332641a3598d852b6880495c0fbb71d6d6
Jesus Rivas <jesuswrivas@gmail.com>
2023-07-12 14:09:58 -0500

    Session, Registration styling and event#index view
    Style sign up form
    
    Style log in form
    
    -Create index method for event controllers
    --The index view now shows all the events created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,45 @@
 class EventsController < ApplicationController
 
+    def new
+        @user = current_user
+        @new_event = current_user.events_created.build
+
+    end
+
+
+    def create
+        @user = current_user
+        @new_event = current_user.events_created.build(event_params)
+
+        if @new_event.save
+            flash[:notice] = "The event was created successfully"
+            redirect_to :root
+
+        else
+            flash.now[:alert] = "Something went wrong. We couldn't create the event"
+            render :new
+
+        end
+
+
+    end
+
+
+
     def index
+        @all_events = Event.all
+
+    end
+
+
+
+
+    private
+
+
+    def event_params
+
+        params.require(:event).permit(:date)
 
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
- root to: "events#index"
+    devise_for :users
+    root to: "events#index"
 
+    resources :events, only: [:show, :index]
+
+    resources :users do
+        resources :events, except: [:show, :index]
+    end
+      
 
 end
