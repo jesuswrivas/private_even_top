9b80378911ef390df3a77775e48921cb0d9916da
Jesus Rivas <jesuswrivas@gmail.com>
2023-07-12 14:47:25 -0500

    User#show view creation
    Create user#show view. It will display all the events the user has created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    @user_events_created = current_user.events_created
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
     devise_for :users
     root to: "events#index"
 
     resources :events, only: [:show, :index]
 
-    resources :users do
+    resources :users, only: [:show] do
         resources :events, except: [:show, :index]
     end
       
